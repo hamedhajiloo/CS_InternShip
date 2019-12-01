11d88ba65b651df8c0ba0c79fe104f04b04f8487
Hamed hajiloo <hamedhajiloo96@gmail.com>
2019-12-01 11:09:20 +0330

    P9 started

---
+++ P9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P9
+{
+    class Program
+    {
+        public delegate int MyDelegate(int a, int b);
+        static void Main(string[] args)
+        {
+            #region 1
+            MyDelegate del = new MyDelegate(Sum);
+            Console.WriteLine(del(9,5));
+            Console.WriteLine(del(19,10));
+            Console.WriteLine("//*****************//");
+
+            MyDelegate del2 = new MyDelegate(Sub);
+            Console.WriteLine(del2(9, 5));
+            Console.WriteLine(del2(19, 10));
+            Console.WriteLine("//*****************//");
+
+            #endregion
+        }
+
+        public static int Sum(int x, int y) => x + y;
+        public static int Sub(int x, int y) => x - y;
+    }
+}
